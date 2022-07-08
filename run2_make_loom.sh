7d58104fc1f2ec19334b0a2eb6ead8088d95e217
eijynagai <eijynagai@gmail.com>
2022-07-08 12:33:31 +0900

    include notebooks

--- run2_make_loom.sh
+++ run2_make_loom.sh
@@ -1,4 +1,3 @@
-# Please adjust the path to the tools and reference accordingly
 sing="singularity exec container/rnakato_singlecell_jupyter.2022.03.sif"
 gtf=references_annotations/Drosophila_melanogaster.BDGP6/Drosophila_melanogaster.BDGP6.32.103.chr_filtered.gtf
 repeat=references_annotations/Drosophila_melanogaster.BDGP6/BDGP6_rmsk.nochr.gtf
@@ -6,5 +5,6 @@ repeat=references_annotations/Drosophila_melanogaster.BDGP6/BDGP6_rmsk.nochr.gtf
 mkdir -p log
 for dir in 1DAE 3DAE
 do
-    $sing velocyto run10x -m $repeat -@ 12 $dir $gtf #>& log/$dir.log &
+    $sing velocyto run10x -m $repeat -@ 12 $dir $gtf >& log/$dir.log &
 done
+
